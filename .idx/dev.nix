aa95b441f7f7e43bd53a04919af3721c7015ed0e
zackelhat <zackelhat@gmail.com>
2025-05-07 12:00:19 +0000

    branch -origin

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,41 +1,60 @@
+
+
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "unstable";
 
   # Use https://search.nixos.org/packages to find packages
-  home.packages = with pkgs; [
-    pkgs.sudo
-    pkgs.python3
-    pkgs.python311Packages.pip
-  #  pkgs.python311Packages.fastapi
-  #  pkgs.python311Packages.uvicorn
-    pkgs.pipenv
-    pkgs.nodejs_20
-    pkgs.nodePackages.nodemon
-    pkgs.gh
-    pkgs.git
-    pkgs.curl
+  packages = [
+      pkgs.nodejs_23
+      pkgs.yarn
+      pkgs.typescript
+     # pkgs.vscode-extensions.ms-azuretools.vscode-docker
+      pkgs.docker_27
+      pkgs.docker-compose
+     # pkgs.docker-engine
+
+      pkgs.sudo
+    #  pkgs.gh
+    #  pkgs.git
   ];
 
   # Sets environment variables in the workspace
   env = {};
-   idx = {
+  idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [ "ms-python.python" "rangav.vscode-thunder-client" ];
+    extensions = [];
+    # Enable previews
+      previews = {
+        # web = {
+        #   # Example: run "npm run start --web" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "start" "--web"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+
+    # Workspace lifecycle hooks
+   /**
     workspace = {
-      # Runs when a workspace is first created with this `dev.nix` file
-      onCreate = {
+       onCreate = {
         create-venv = ''
-          python -m venv $HOME/.venv/
-          source $HOME/.venv//bin/activate
-          pip install -r requirements.txt
+          npm ci --production
         '';
         # Open editors for the following files by default, if they exist:
-        default.openFiles = [ "app.py" ];
+        default.openFiles = [ "README.md" ];
       };
-      # To run something each time the workspace is (re)started, use the `onStart` hook
+       onStart = {
+        # Example: start a background task to watch and re-build backend code
+         watch-backend = "npm run start --web";
+      };
     };
+  */
   };
 }
