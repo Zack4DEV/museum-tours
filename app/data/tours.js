a99784de7ade367827aa73713babb9881d323603
zakaria.elhatimi <zakaria.elhatimi@etu.uae.ac.ma>
2025-12-02 23:25:01 +0000

    branch - origin

--- app/data/tours.js
+++ app/data/tours.js
@@ -4,20 +4,20 @@ export const tours = [
     title: 'Ancient Civilizations Tour',
     duration: '2 hours',
     rating: 4.8,
-    image: 'https://images.unsplash.com/photo-1518998053901-5348d3961a04',
+    image: 'https://images.unsplash.com/photo-1563000215-e31a8ddcb2d0',
   },
   {
     id: 2,
     title: 'Renaissance Art Highlights',
     duration: '1.5 hours',
     rating: 4.9,
-    image: 'https://images.unsplash.com/photo-1547909997-6a757c2a71e2',
+    image: 'https://images.unsplash.com/photo-1564399579883-451a5d44ec08',
   },
   {
     id: 3,
     title: 'Modern Art Movements',
     duration: '1 hour',
     rating: 4.7,
-    image: 'https://images.unsplash.com/photo-1561214041-8e0b11542f2b',
+    image: 'https://images.unsplash.com/photo-1630002932129-0ceb86dda840',
   },
 ];
